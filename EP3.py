9052cbe70b87c0427860e060760bd6517da098ba
Patrick8991 <patricksw0104@hotmail.com>
2016-04-22 13:06:14 -0300

    Segunda Programação

--- EP3.py
+++ EP3.py
@@ -1,165 +1,50 @@
 import tkinter as tk
-import numpy as np
 
-class Game:
+class Tic_Tac_Toe:
     
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry("350x400+400+400")
-        self.window.rowconfigure(0,minsize=100,weight=1)
-        self.window.rowconfigure(1,minsize=100,weight=1)
-        self.window.rowconfigure(2,minsize=100,weight=1)
-        self.window.rowconfigure(3,minsize=50,weight=1)
-        self.window.columnconfigure(0,minsize=100,weight=1)
-        self.window.columnconfigure(1,minsize=100,weight=1)
-        self.window.columnconfigure(2,minsize=100,weight=1)
         
-        self.botão_0_0 = tk.Button(self.window)
-        self.botão_0_0.grid(row=0,column=0,sticky='nsew')
-        self.botão_0_0.configure(command=self.botão_0_0_clicado)
+        for i in range(4):
+            if i < 3:
+                self.window.rowconfigure(i,minsize=100,weight=1)
+                self.window.columnconfigure(i,minsize=100,weight=1)
+            else:
+                self.window.rowconfigure(i,minsize=50,weight=1)
+                
+        self.buttons = []
         
-        self.botão_0_1 = tk.Button(self.window)
-        self.botão_0_1.grid(row=0,column=1,sticky='nsew')
-        self.botão_0_1.configure(command=self.botão_0_1_clicado)
         
-        self.botão_0_2 = tk.Button(self.window)
-        self.botão_0_2.grid(row=0,column=2,sticky='nsew')
-        self.botão_0_2.configure(command=self.botão_0_2_clicado)
-        
-        self.botão_1_0 = tk.Button(self.window)
-        self.botão_1_0.grid(row=1,column=0,sticky='nsew')
-        self.botão_1_0.configure(command=self.botão_1_0_clicado)
-        
-        self.botão_1_1 = tk.Button(self.window)
-        self.botão_1_1.grid(row=1,column=1,sticky='nsew')
-        self.botão_1_1.configure(command=self.botão_1_1_clicado)
-        
-        self.botão_1_2 = tk.Button(self.window)
-        self.botão_1_2.grid(row=1,column=2,sticky='nsew')
-        self.botão_1_2.configure(command=self.botão_1_2_clicado)
-        
-        self.botão_2_0 = tk.Button(self.window)
-        self.botão_2_0.grid(row=2,column=0,sticky='nsew')
-        self.botão_2_0.configure(command=self.botão_2_0_clicado)
-        
-        self.botão_2_1 = tk.Button(self.window)
-        self.botão_2_1.grid(row=2,column=1,sticky='nsew')
-        self.botão_2_1.configure(command=self.botão_2_1_clicado)
-        
-        self.botão_2_2 = tk.Button(self.window)
-        self.botão_2_2.grid(row=2,column=2,sticky='nsew')
-        self.botão_2_2.configure(command=self.botão_2_2_clicado)
-        
+        for i in range(3):
+            for j in range(3):
+                self.b = tk.Button(self.window)
+                if i == 0:
+                    z=0
+                elif i == 1:
+                    z=3
+                elif i == 2:
+                    z=6
+                self.b.configure(command=lambda x = z+j: self.botao_clicado(x))
+                self.b.grid(row=i, column=j, sticky="nsew")
+                self.buttons.append(self.b)
+                
         self.label_status = tk.Label()
         self.label_status.configure(text="Tic Tac Toe")
         self.label_status.grid(row=3,column=0,columnspan=3)
-        
+            
         self.turn = 1
-        self.n = input("Qual é o nome do primeiro jogador?")
-        self.n2 = input("Qual é o nome do segundo jogador?")
-        #self.jogador.configure(command=self.jogador_TTT)
-                
         
-    def iniciar(self):
+    def start(self):
         self.window.mainloop()
         
-    #def jogador_TTT(self):
-    #    if self.turn%2 != 0:
-    #        self.jogador = "X"
-    #    else:
-    #        self.jogador = "O"
-    
-    def botão_0_0_clicado(self):
-        #self.botão_clicado(0,0)
-        if self.turn%2 != 0:
-            self.botão_0_0.configure(text="{0}".format(self.n[0]))
+    def botao_clicado(self,x):
+        if self.turn % 2 != 0:
+            self.buttons[x].configure(text="X")
         else:
-            self.botão_0_0.configure(text="{0}".format(self.n2[0]))
-        self.turn+=1
-        self.botão_0_0.configure(state="disabled")
+            self.buttons[x].configure(text="O")
+        self.turn += 1
+        self.buttons[x].configure(state="disabled")
         
-    def botão_0_1_clicado(self):
-        #self.botão_clicado(0,1)
-        if self.turn%2 != 0:
-            self.botão_0_1.configure(text="{0}".format(self.n[0]))
-        else:
-            self.botão_0_1.configure(text="{0}".format(self.n2[0]))
-        self.turn+=1
-        self.botão_0_1.configure(state="disabled")
-        
-    def botão_0_2_clicado(self):
-        #self.botão_clicado(0,2)
-        if self.turn%2 != 0:
-            self.botão_0_2.configure(text="{0}".format(self.n[0]))
-        else:
-            self.botão_0_2.configure(text="{0}".format(self.n2[0]))
-        self.turn+=1
-        self.botão_0_2.configure(state="disabled")
-        
-    def botão_1_0_clicado(self):
-        #self.botão_clicado(0,0)
-        if self.turn%2 != 0:
-            self.botão_1_0.configure(text="{0}".format(self.n[0]))
-        else:
-            self.botão_1_0.configure(text="{0}".format(self.n2[0]))
-        self.turn+=1
-        self.botão_1_0.configure(state="disabled")
-        
-    def botão_1_1_clicado(self):
-        #self.botão_clicado(1,1)
-        if self.turn%2 != 0:
-            self.botão_1_1.configure(text="{0}".format(self.n[0]))
-        else:
-            self.botão_1_1.configure(text="{0}".format(self.n2[0]))
-        self.turn+=1
-        self.botão_1_1.configure(state="disabled")
-        
-    def botão_1_2_clicado(self):
-        #self.botão_clicado(1,2)
-        if self.turn%2 != 0:
-            self.botão_1_2.configure(text="{0}".format(self.n[0]))
-        else:
-            self.botão_1_2.configure(text="{0}".format(self.n2[0]))
-        self.turn+=1
-        self.botão_1_2.configure(state="disabled")
-        
-    def botão_2_0_clicado(self):
-        #self.botão_clicado(2,0)
-        if self.turn%2 != 0:
-            self.botão_2_0.configure(text="{0}".format(self.n[0]))
-        else:
-            self.botão_2_0.configure(text="{0}".format(self.n2[0]))
-        self.turn+=1
-        self.botão_2_0.configure(state="disabled")
-        
-    def botão_2_1_clicado(self):
-        #self.botão_clicado(2,1)
-        if self.turn%2 != 0:
-            self.botão_2_1.configure(text="{0}".format(self.n[0]))
-        else:
-            self.botão_2_1.configure(text="{0}".format(self.n2[0]))
-        self.turn+=1
-        self.botão_2_1.configure(state="disabled")
-        
-    def botão_2_2_clicado(self):
-        #self.botão_clicado(2,2)
-        if self.turn%2 != 0:
-            self.botão_2_2.configure(text="{0}".format(self.n[0]))
-        else:
-            self.botão_2_2.configure(text="{0}".format(self.n2[0]))
-        self.turn+=1
-        self.botão_2_2.configure(state="disabled")
-        
-    #def texto(self):
-    #    if self.turn%2 != 0:
-    #        return ("Vez do X")
-    #    elif self.turn%2 == 0:
-    #        return ("Vez do O")
-    #    elif self.turn == -1:
-    #        return ("O jogador {0} ganhou!".format(self.jogador))
-    #    elif self.turn == -2:
-    #        return ("Deu velha!")
-        
-        
-jogo = Game()
-jogo.iniciar()
+jogo = Tic_Tac_Toe()
+jogo.start()
