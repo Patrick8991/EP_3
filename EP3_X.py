ceff9c5aa709acbf3395f4e2e0b474715e77fc40
Patrick8991 <patricksw0104@hotmail.com>
2016-04-27 00:10:55 -0300

    Oitava Programção

--- EP3_X.py
+++ EP3_X.py
@@ -42,10 +42,12 @@ class Game:
         else:
             return -1
     
-    def restart_match(self):
+    def clean_game(self):
         if self.verify_winner()==1 or self.verify_winner()==2 or self.verify_winner()==0:
             return False
         return True
+        
+    
 
 class Tabuleiro:
     
@@ -72,7 +74,7 @@ class Tabuleiro:
         self.botao_nomes = tk.Button(self.window,width=10,height=4,text="JOGAR")
         self.botao_nomes.configure(command=self.nomes)
         self.botao_nomes.grid(padx=140, pady=50)
-        
+                
     def tabuleiro(self):
         self.game = Game()
                 
@@ -99,17 +101,36 @@ class Tabuleiro:
                 self.buttons.append(self.b)
                 
         self.label_status = tk.Label()
-        self.label_status.configure(text="Tic Tac Toe", font="BodoniMTBlack 22 bold")
+        self.label_status.configure(text="Tic Tac Toe", font="BodoniMTBlack 22 bold",bg="yellow")
         self.label_status.grid(row=3,column=0,columnspan=3)
         
         self.label_player = tk.Label()
         self.label_player.configure(text="Vez de {0}".format(self.n1), font="BrandleyHandITC 18")
         self.label_player.grid(row=4,column=0,columnspan=3)
         
-        self.botao_reiniciar = tk.Button()
-        self.botao_reiniciar.configure(text="Reiniciar")
-        self.botao_reiniciar.configure(command=self.rematch)
-        self.botao_reiniciar.grid(row=5,column=0,columnspan=3)
+        self.botao_revanche = tk.Button()
+        self.botao_revanche.configure(text="Rematch",height=2,width=10,state="disabled")
+        self.botao_revanche.configure(command=self.rematch_tabuleiro)
+        self.botao_revanche.grid(row=5,column=1)
+        
+        self.placar_X = tk.Label()
+        self.placar_X.configure(text="{0}".format(self.n1),font="Arial 16 bold")
+        self.placar_X.grid(row=3,column=0)
+        
+        self.placar_O = tk.Label()
+        self.placar_O.configure(text="{0}".format(self.n2),font="Arial 16 bold")
+        self.placar_O.grid(row=3,column=2)
+        
+        self.ponto_X = tk.Label()
+        self.ponto_X.configure(text="0")
+        self.ponto_X.grid(row=4,column=0)
+        
+        self.ponto_O = tk.Label()
+        self.ponto_O.configure(text="0")
+        self.ponto_O.grid(row=4,column=2)
+        
+        self.X=0
+        self.O=0
             
     def nomes(self):
         self.n1 = self.nome1.get()
@@ -120,7 +141,6 @@ class Tabuleiro:
         self.text2.grid_forget()
         self.botao_nomes.grid_forget()
         self.tabuleiro()
-        
         
     def start(self):
         self.window.mainloop()
@@ -139,17 +159,27 @@ class Tabuleiro:
     def end_match(self):
         if self.game.verify_winner()==1:
             self.label_player.configure(text="{0} ganhou!".format(self.n1), font="BrandleyHandITC 18")
+            self.X+=1
+            self.botao_revanche.configure(state="active")
             for r in range(9):
                 self.buttons[r].configure(state="disabled")
         elif self.game.verify_winner()==2:
             self.label_player.configure(text="{0} ganhou!".format(self.n2), font="BrandleyHandITC 18")
+            self.O+=1
+            self.botao_revanche.configure(state="active")
             for r in range(9):
                 self.buttons[r].configure(state="disabled")
         elif self.game.verify_winner()==0:
             self.label_player.configure(text="Deu Velha!", font="BrandleyHandITC 18")
+            self.botao_revanche.configure(state="active")
+        self.pontuacao()
+        
+    def pontuacao(self):
+        self.ponto_X.configure(text="{0}".format(self.X))
+        self.ponto_O.configure(text="{0}".format(self.O))
     
-    def rematch(self):
-        if self.game.restart_match()==False:
+    def rematch_tabuleiro(self):
+        if self.game.clean_game()==False:
             for r in range(9):
                 self.buttons[r].configure(text="",state="active")
             self.label_player.configure(text="Vez de {0}".format(self.n1), font="BrandleyHandITC 18")
